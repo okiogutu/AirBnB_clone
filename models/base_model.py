20b1214682cf89bc072cbbf150067a51ea1922aa
wosohd <wosohd@gmail.com>
2023-07-15 03:12:16 -0700

    -m models

---
+++ models/base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Class BaseModel"""
+from uuid import uuid4
+from datetime import datetime
+import models
+
+class BaseModel:
+    """Constructs the BaseModel of the HBnB project."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel."""
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
+    def save(self):
+        """Updates the public instance attribute
+        with the current datetime"""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def __str__(self):
+        """Returns a readable string representation
+        of BaseModel instances"""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+     def to_dict(self):
+        """Returns a dictionary that contains all
+        keys/values of the instance"""
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
